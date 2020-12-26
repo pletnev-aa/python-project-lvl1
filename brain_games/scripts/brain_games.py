32a72d00bd8b7ed328250afe03d051f72d778fbd
pletnev-aa <apletnev@list.ru>
2020-12-26 14:37:20 +0400

    fix: add import cli

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
 
 def greet():
-    print ('Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+    cli.welcome_user()
+
 
 def main():
     greet()
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
