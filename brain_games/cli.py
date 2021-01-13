9948847392411e21742c6c5a3510551ea7b41d18
pletnev-aa <apletnev@list.ru>
2021-01-14 01:05:55 +0400

    fix: cli and brain-games

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,6 +3,7 @@ import prompt
 
 
 def welcome_user():
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     return print('Hello, {}!'.format(name))
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,7 +3,6 @@ from brain_games import cli
 
 
 def main():
-    print('Welcome to the Brain Games!')
     return cli.welcome_user()
 
 
