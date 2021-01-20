78ccc3c45aa1ad46ef7231e0bee09c2122521380
pletnev-aa <apletnev@list.ru>
2021-01-20 20:11:30 +0400

    fix cli and brain-even in directory scripts

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from random import randint, choice
 import prompt
 
 
@@ -9,5 +10,41 @@ def welcome_user():
     return name
 
 
+def status(*args):
+    args = args[0]
+    answer = args[1]
+    question = args[0]
+    # print(answer)
+    b = "'{}' is wrong answer; (. Correct answer was '{}'."
+    print('Question: {}'.format(question))
+    if type(answer) is int:
+        user_answer = prompt.integer('Your answer: ')
+        if user_answer == answer:
+            print("Correct!")
+            return True
+        else:
+            return print(b.format(user_answer, answer))
+    else:
+        user_answer = prompt.string('Your answer: ')
+        if user_answer == answer:
+            print("Correct!")
+            return True
+        else:
+            return print(b.format(user_answer, answer))
+
+
+def question_answer():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    char = '+-*'
+    operation = choice(char)
+    question = '{} {} {}'.format(num_1, operation, num_2)
+    answer_even = 'yes' if num_1 % 2 == 0 else 'no'
+    answer_calc = eval(question)
+    return question, answer_calc, num_1, answer_even
+
+
 if __name__ == '__main__':
     welcome_user()
+    status()
+    question_answer()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
-import random
-import prompt
-from brain_games import cli
+from brain_games.games import game_even
 
 
 def main():
-    name = cli.welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    index = 0
-    while index <= 3:
-        num = random.randint(1, 100)
-        print('Question: {}'.format(num))
-        answer = prompt.string('Your answer: ')
-        correct_answer = 'yes' if num % 2 == 0 else 'no'
-        if num % 2 == 0 and answer == 'yes' or num % 2 != 0 and answer == 'no':
-            print("Correct!")
-            index = index + 1
-            if index == 3:
-                return print("Congratulations, {}!".format(name))
-        else:
-            print(
-                "'{}' is wrong answer; (."
-                " Correct answer was '{}'.".format(answer, correct_answer))
-            break
+    return game_even.run()
 
 
 if __name__ == '__main__':
