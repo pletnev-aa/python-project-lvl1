b55224d5f64ed05a0711ea5aef78737b5b0a5d77
pletnev-aa <apletnev@list.ru>
2021-01-26 18:31:59 +0400

    fix brain-progression

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games import engine
+from random import randint
 
 
 def run(game):
-    status = cli.status(game)
+    status = engine.status(game)
     if status:
         name = status[1]
         return print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
-    run('progression')
+def get_question_answer():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    start = min(num_1, num_2)
+    step = max(num_1, num_2) - start
+    num = randint(5, 10)
+    stop = start + (step * num)
+    progression = list(range(start, stop, step))
+    len_progression = len(progression) - 1
+    hidden_num = randint(1, len_progression)
+    answer = progression[hidden_num]
+    progression[hidden_num] = '..'
+    question = ' '.join(map(str, progression))
+    return question, answer
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -3,7 +3,7 @@ from brain_games.games import game_progression
 
 
 def main():
-    return game_progression.run('progression')
+    return game_progression.run(game_progression)
 
 
 if __name__ == '__main__':
