29cc7c81a2ad33a6f4a4f60d04c1395fd63a1932
pletnev-aa <apletnev@list.ru>
2021-01-26 18:27:54 +0400

    fix brain-calc

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games import engine
+from random import randint, choice
 
 
 def run(game):
-    status = cli.status(game)
+    status = engine.status(game)
     if status:
         name = status[1]
         return print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
-    run('calc')
+def get_question_answer():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    operation = choice('+-*')
+    question = '{} {} {}'.format(num_1, operation, num_2)
+    answer = eval(question)
+    return question, answer
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,7 +3,7 @@ from brain_games.games import game_calc
 
 
 def main():
-    return game_calc.run('calc')
+    return game_calc.run(game_calc)
 
 
 if __name__ == '__main__':
