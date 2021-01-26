d63cd5466b60623715b194c0ff4086566a0070b7
pletnev-aa <apletnev@list.ru>
2021-01-26 18:29:37 +0400

    fix brain-gcd

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games import engine
+from random import randint
 
 
 def run(game):
-    status = cli.status(game)
+    status = engine.status(game)
     if status:
         name = status[1]
         return print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
-    run('gcd')
+def get_question_answer():
+    num_1 = randint(1, 100)
+    num_2 = randint(1, 100)
+    question = '{} {}'.format(num_1, num_2)
+    while max(num_1, num_2) % min(num_1, num_2) != 0:
+        if num_1 > num_2:
+            num_1 = num_1 % num_2
+        elif num_1 < num_2:
+            num_2 = num_2 % num_1
+    answer = min(num_1, num_2)
+    return question, answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,7 +3,7 @@ from brain_games.games import game_gcd
 
 
 def main():
-    return game_gcd.run('gcd')
+    return game_gcd.run(game_gcd)
 
 
 if __name__ == '__main__':
