b7626dc18e573e0b53c8e625db26dac6e55a8bbb
pletnev-aa <apletnev@list.ru>
2021-01-26 18:31:00 +0400

    fix brain-prime

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games import engine
+from random import randint
 
 
 def run(game):
-    status = cli.status(game)
+    status = engine.status(game)
     if status:
         name = status[1]
         return print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
-    run('prime')
+def get_question_answer():
+    num_1 = randint(1, 100)
+    divisor = 2
+    remainder = 0
+    if num_1 > 1:
+        while True:
+            if divisor * divisor <= num_1 and remainder != 1:
+                if num_1 % divisor == 0:
+                    remainder = remainder + 1
+                divisor = divisor + 1
+            elif remainder == 1:
+                answer = 'no'
+                question = num_1
+                return question, answer
+            else:
+                answer = 'yes'
+                question = num_1
+                return question, answer
+    else:
+        answer = 'no'
+        question = num_1
+        return question, answer
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,7 +3,7 @@ from brain_games.games import game_prime
 
 
 def main():
-    return game_prime.run('prime')
+    return game_prime.run(game_prime)
 
 
 if __name__ == '__main__':
