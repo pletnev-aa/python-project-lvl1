742a670fe50192323fe2dc1de8b32bf1d3506201
pletnev-aa <apletnev@list.ru>
2021-01-26 18:28:48 +0400

    fix brain-even

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games import engine
+from random import randint
 
 
 def run(game):
-    status = cli.status(game)
+    status = engine.status(game)
     if status:
         name = status[1]
         return print("Congratulations, {}!".format(name))
 
 
-if __name__ == '__main__':
-    run('even')
+def get_question_answer():
+    num_1 = randint(1, 100)
+    answer = 'yes' if num_1 % 2 == 0 else 'no'
+    question = num_1
+    return question, answer
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,7 +3,7 @@ from brain_games.games import game_even
 
 
 def main():
-    return game_even.run('even')
+    return game_even.run(game_even)
 
 
 if __name__ == '__main__':
